ed6a3f371f8371e5f416e61f39bab12ad70ba649
Aleksandr Mirashnichenko <sasha.bumail.bu@gmail.com>
2016-08-01 20:35:18 +0300

    First commit

---
+++ MyApplication/app/src/main/java/com/example/aliaksandrmirashnichenka/mediaplayer/Fragments/PlayerFragment.java
@@ -0,0 +1,12 @@
+package com.example.aliaksandrmirashnichenka.mediaplayer.Fragments;
+
+import android.support.v4.app.Fragment;
+
+import org.androidannotations.annotations.EFragment;
+
+/**
+ * Created by aliaksandrmirashnichenka on 01.08.16.
+ */
+@EFragment
+public class PlayerFragment extends Fragment {
+}
